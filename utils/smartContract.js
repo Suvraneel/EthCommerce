8a6c3e35f19a3d24b718e9960aea55663534564f
Saviour1001 <harshghodkar@gmail.com>
2023-02-18 16:01:28 +0530

    Update smartContract.js

--- utils/smartContract.js
+++ utils/smartContract.js
@@ -8,15 +8,21 @@ import { useNetwork } from "wagmi";
 
 export const callContract = async (title, description, price, image) => {
   if (ethereum) {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    try {
+      // request for account access
+      await ethereum.request({ method: "eth_requestAccounts" });
+    } catch {
+      console.log("User denied account access");
+    }
+    const provider = new ethers.providers.Web3Provider(ethereum);
     console.log("provider", provider);
     const signer = provider.getSigner();
     console.log("signer", signer);
     const contract = new ethers.Contract(
       // Polygon: 0x441d78a685da0dA6623363965Ab0AaF499Ba42dc
       // Mantle: 0x039a8561e235cf960bfed66aad74441e3594abb4
-      "0xC651cda1575c6da36C43751cAaC30f757b96f8eC",
-      HARSH_ABI,
+      "0xD0B8a123377C79eC69a63885485eC5AB202960b8",
+      FACTORY_CONTRACT_ABI,
       signer
     );
     console.log("contract", contract);
@@ -27,7 +33,7 @@ export const callContract = async (title, description, price, image) => {
         "SOMETHING",
         100,
         10000,
-        "SOMETHING"
+        "0x039a8561e235cf960bfed66aad74441e3594abb4"
       );
       console.log("tx", tx);
       const receipt = await tx.wait();
