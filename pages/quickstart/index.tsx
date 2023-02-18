93dfb429d3f42f72b7c2b345b5c82a7404334c83
Suvraneel Bhuin <bsuvraneel@gmail.com>
2023-02-18 23:17:50 +0530

    home readme & quickstart enhanced :octokit:

--- pages/quickstart/index.tsx
+++ pages/quickstart/index.tsx
@@ -38,56 +38,81 @@ const Quickstart: NextPage = () => {
                         </p><p>
                             We make it easy to list your avatar or AR character on our platform and connect with potential buyers who are looking for unique and personalized virtual identities. Our platform provides a secure and transparent marketplace for transactions, so you can sell your avatar or AR character with confidence.
                         </p><p>
-                            In addition, we offer a range of features to help you manage your sales and transactions, including order management, payment processing, and dispute resolution. Our goal is to make it as easy as possible for you to monetize your virtual self and turn your passion for virtual customization into real profit.
+                            In addition, we offer a range of features to help you manage your sales and transactions, including order management, payment processing, and dispute resolution.
+                        </p><p>
+                            So what are you waiting for? Sign up for our Avatar and AR Character Marketplace today and start turning your virtual self into real profit. Join the growing community of creators who are monetizing their virtual identities and making a name for themselves in the Metaverse and AR world.
+                        </p>
+                    </div>
+                </div>
+                <div className="w-full h-fit flex flex-row justify-start bg-white rounded-md border-2 border-black px-10 py-8 relative snap-center snap-always">
+                    <div className="flex flex-col justify-start gap-4 text-xl w-full sm:w-3/5">
+                        <h1 className='text-4xl font-bold'>Strike the Right Note with Your Music: Sell Your Royalty and Copyrights Today!</h1>
+                        <p>
+                            Our Royalty and Copyrights Marketplace offers you the opportunity to monetize your music and create a new revenue stream. By selling your music rights on our platform, you can unlock the true value of your creative work and get paid for your hard work and talent, even after the initial sale.
+                        </p><p>
+                            We make it easy to list your music on our platform and connect with potential buyers who are interested in purchasing the rights to your music. Our platform provides a secure and transparent marketplace for transactions, so you can sell your music with confidence. We also offer a range of tools and resources to help you set your price and promote your music to a wider audience.
+                        </p><p>
+                            When you sell your music copyright, you allow the buyer to use your music in a variety of ways, such as using it in a film, commercial, or other creative work. In exchange, you receive royalties for every use of your music, ensuring that you continue to get paid for your creative work.
+                        </p><p>
+                            So why wait? Join the growing community of musicians who are monetizing their creative work and making a name for themselves in the music industry.
+                        </p>
+                    </div>
+                    <div className='flex w-2/5 h-full overflow-hidden flex-col justify-center item-center'>
+                        <SplineObject scene={'https://prod.spline.design/Z7TZb600GdFrdW3V/scene.splinecode'} className='' />
+                    </div>
+                </div>
+                <div className="w-full h-fit flex flex-row justify-end bg-white rounded-md border-2 border-black px-10 py-8 relative snap-center snap-always">
+                    <div className='flex w-2/5 h-full overflow-hidden flex-col justify-center item-center'>
+                        <SplineObject scene={'https://prod.spline.design/Zdp6DU73GiQ7ezWn/scene.splinecode'} className='' />
+                    </div>
+                    <div className="flex flex-col justify-start gap-4 text-xl w-full sm:w-3/5">
+                        <h1 className='text-4xl font-bold'>Turn Your Passion into Profit: Sell Your Comics, Art, Courses, Newsletter and More with Ease!</h1>
+                        <p>
+                            Are you passionate about creating comics, art, courses, or newsletters? With our platform, you can turn that passion into profit by selling your creations to a global audience. Our platform is designed to make it easy for you to build a loyal following, create engaging content, and monetize your work with a variety of payment options.
+                        </p><p>
+                            With our platform, you can build a loyal following with simple posts, email newsletters, and automated workflows. Our powerful tools make it easy for you to create engaging content that resonates with your audience and keeps them coming back for more. You can also use our platform to monetize your content with a variety of payment options, including letting your customers pay what they want or choose between one-time, recurring, or fixed-length payments in your currency of choice. And we’ll handle the fine print, like VAT, so you can focus on what you do best - creating.
+                        </p><p>
+                            So what are you waiting for? Join the growing community of creators who are using our platform to build their businesses and make a name for themselves in the world of comics, art, courses, and more.
                         </p>
                     </div>
                 </div>
                 <div className="w-full h-fit flex flex-col justify-start items-start bg-white rounded-md border-2 border-black snap-center snap-always">
                     <div className="text-lg px-10 py-6">
-                        <p className="text-lg">
-                            In this guide, we&apos;ll walk you through the steps to get started in this exciting new economy where you can sell your courses, NFTs, services, audio, royalty, art, and more.
-                        </p>
+                        <h2 className="text-2xl font-semibold">
+                            Let&apos;s walk you through the steps to get started in this exciting new economy.
+                        </h2>
                         <ol>
                             <li>
-                                <h3 className='font-bold'>
-                                    Step 1: Acquire Tokens
-                                </h3>
-                                To participate in the Creator Economy, you&apos;ll first need to acquire tokens. Tokens can be purchased on exchanges or directly from the platform.
+                                <h3 className='font-bold'>Sign up:</h3>
+                                To get started, sign up for an account on our website. It&apos;s free and easy to do.
+                            </li>
+                            <li>
+                                <h3 className='font-bold'>Create your listing:</h3>
+                                Once you&apos;ve signed up, you can create a listing for your comic books, art, courses, newsletters, or any other digital creations you want to sell. Be sure to add a detailed description, pricing information, and any other details that potential buyers may need to know.
                             </li>
                             <li>
                                 <h3 className='font-bold'>
-                                    Step 2: Set up your Creator Profile
+                                    Customize your storefront:
                                 </h3>
-                                Once you have your tokens, you can set up your Creator profile on the platform. This is where you&apos;ll be able to showcase your skills, interests, and projects. Make sure to include all relevant information about yourself to help others find you and your offerings.
+                                Personalize your storefront by adding your logo, banner, and customizing your colors and fonts. This will help make your store more memorable and recognizable to your customers.
+                            </li>
+                            <li>
+                                <h3 className='font-bold'>Promote your store:</h3>
+                                Share your store on social media and other marketing channels to get the word out about your creations. Use our built-in marketing tools to create email campaigns, social media posts, and other promotional materials.
                             </li>
                             <li>
                                 <h3 className='font-bold'>
-                                    Step 3: Create and Upload Your Offerings
+                                    Engage with your customers:
                                 </h3>
-                                <p className="text-lg">
-                                    The Creator Economy is all about sharing your talents and offerings with the world. You can sell anything from courses, NFTs, services, audio, royalty, art, and more. Once you&apos;ve created your offerings, simply upload them to the platform and set your price in tokens.
-                                </p>
+                                Respond to customer inquiries promptly and provide excellent customer service to build trust and loyalty. This will help you create a positive reputation and increase your sales.
                             </li>
                             <li>
-                                <h3 className='font-bold'>
-                                    Step 4: Market and Promote Your Offerings
-                                </h3>
-                                <p className="text-lg">
-                                    To attract buyers, you&apos;ll need to market and promote your offerings. Use the platform&apos;s marketing tools and social media to get the word out about your offerings. Engage with potential buyers and build relationships to create a loyal customer base.
-                                </p>
+                                <h3 className='font-bold'>Get paid:</h3>
+                                We offer a range of payment options to make it easy for your customers to pay you. Choose between one-time, recurring, or fixed-length payments in your currency of choice. We handle the fine print, like VAT, so you don&apos;t have to worry about it.
                             </li>
                             <li>
-                                <h3 className='font-bold'>
-                                    Step 5: Get Rewarded
-                                </h3>
-                                <p className="text-lg">
-
-                                    When someone purchases your offering, you&apos;ll be rewarded with tokens. These tokens can then be sold or used to participate in other offerings on the platform. Make sure to track your sales and adjust your pricing as necessary to maximize your earnings.
-                                </p>
-                                <p className="text-lg">
-                                    And that&apos;s it! With these five simple steps, you&apos;re on your way to becoming a part of the Token Gated Creator Economy. So why wait? Start creating and sharing your offerings today and take your creativity to the next level!
-                                </p>
-                            </li>
+                                <h3 className='font-bold'>Grow your business:
+                                </h3> Use our analytics, customer management, and marketing tools to track your sales, engage with your customers, and grow your business over time. The more you use our platform, the more you can learn about your customers and what they want, which can help you create even better products and services in the future.</li>
                         </ol>
                     </div>
                 </div>
